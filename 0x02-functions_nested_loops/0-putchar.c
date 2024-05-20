808faa92104ed0dd7972bcadd901c572435b1d35
chimamka27 <chimamaka220@gmail.com>
2024-05-20 05:13:06 -0700

    added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,10 @@
-#include "main.h"
-
+#include<stdio.h>
 /**
-* main - print _putchar
-* is written by Favour
-* Return: 0
+* main - Print
+* Return: 0. Successful.
 */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,26 @@
+#include <stdio.h>
 /**
- * main - finds and prints the sum of the even-valued terms
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - main function
+ *
+ * Return: nothing
  */
 int main(void)
 {
-	int i;
-	unsigned long int j, k, next, sum;
+int counter = 0;
+long int a = 1;
+long int b = a;
+long int c = a + b;
 
-	j = 1;
-	k = 2;
-	sum = 0;
-
-	for (i = 1; i <= 33; ++i)
-	{
-		if (j < 4000000 && (j % 2) == 0)
-		{
-			sum = sum + j;
-		}
-		next = j + k;
-		j = k;
-		k = next;
-	}
-
-	printf("%lu\n", sum);
-
-	return (0);
+while (c < 4000000)
+{
+if (c % 2 == 0)
+{
+counter += c;
+}
+a = b;
+b = c;
+c = a + b;
+}
+printf("%d\n", counter);
+return (0);
 }
